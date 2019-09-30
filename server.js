05bd303d6e5d6728691ecb6202e45a6a773b5ceb
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-09-30 23:54:43 +0700

    [UPDATE] add express

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 
-const http = require('http');
+const express = require('express')
+const app = express()
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World\n');
+// });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+app.get('/app', function (req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Hello World!')
+})
